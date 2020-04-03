1aa9009bda740a67ef6936a86bd0d2607d07df82
muhamadhikayat <50065133+muhamadhikayat@users.noreply.github.com>
2020-04-03 16:13:24 +0700

    Add files via upload

---
+++ SBDL/Interface/surat/admin.php
@@ -0,0 +1,118 @@
+<?php
+    ob_start();
+    //cek session
+    session_start();
+
+    if(empty($_SESSION['admin'])){
+        $_SESSION['err'] = '<center>Anda harus login terlebih dahulu!</center>';
+        header("Location: ./");
+        die();
+    } else {
+?>
+<!doctype html>
+<html lang="en">
+
+<!-- Include Head START -->
+<?php include('include/head.php'); ?>
+<!-- Include Head END -->
+
+<!-- Body START -->
+<body class="bg">
+
+<!-- Header START -->
+<header>
+
+<!-- Include Navigation START -->
+<?php include('include/menu.php'); ?>
+<!-- Include Navigation END -->
+
+</header>
+<!-- Header END -->
+
+<!-- Main START -->
+<main>
+
+    <!-- container START -->
+    <div class="container">
+
+    <?php
+        if(isset($_REQUEST['page'])){
+            $page = $_REQUEST['page'];
+            switch ($page) {
+                case 'tsm':
+                    include "transaksi_surat_masuk.php";
+                    break;
+                case 'ctk':
+                    include "cetak_disposisi.php";
+                    break;
+                case 'tsk':
+                    include "transaksi_surat_keluar.php";
+                    break;
+                case 'asm':
+                    include "agenda_surat_masuk.php";
+                    break;
+                case 'ask':
+                    include "agenda_surat_keluar.php";
+                    break;
+                case 'ref':
+                    include "referensi.php";
+                    break;
+                case 'sett':
+                    include "pengaturan.php";
+                    break;
+                case 'pro':
+                    include "profil.php";
+                    break;
+                case 'gsm':
+                    include "galeri_sm.php";
+                    break;
+                case 'gsk':
+                    include "galeri_sk.php";
+                    break;
+            }
+        } else {
+    ?>
+        <!-- Row START -->
+        <div class="row">
+
+            <!-- Welcome Message START -->
+            <div class="col s12">
+                <div class="card bg-color blue;">
+                    <div class="card-content">
+                        <h4>Selamat Datang <?php echo $_SESSION['nama']; ?></h4>
+                        <p class="description">Anda login sebagai
+                        <?php
+                            if($_SESSION['admin'] == 1){
+                                echo "<strong>Super Admin</strong>. Anda memiliki akses penuh terhadap sistem.";
+                            } elseif($_SESSION['admin'] == 2){
+                                echo "<strong>Administrator</strong>. Berikut adalah statistik data yang tersimpan dalam sistem.";
+                            } else {
+                                echo "<strong>Petugas Disposisi</strong>. Berikut adalah statistik data yang tersimpan dalam sistem.";
+                            }?></p>
+                    </div>
+                </div>
+            </div>
+            <!-- Welcome Message END -->
+        </div>
+        <!-- Row END -->
+    <?php
+        }
+    ?>
+    </div>
+    <!-- container END -->
+
+</main>
+<!-- Main END -->
+
+<!-- Include Footer START -->
+<?php include('include/footer.php'); ?>
+<!-- Include Footer END -->
+
+</body>
+<!-- Body END -->
+
+</html>
+
+<?php
+    }
+?>
